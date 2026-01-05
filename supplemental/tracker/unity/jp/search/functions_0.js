a37c84eae34831a0a1967ff6669dafa2cf8e1095
Kazuki Kyakuno <kyakuno@axell.co.jp>
2026-01-05 17:58:58 +0900

    Improve documentation

--- supplemental/tracker/unity/jp/search/functions_0.js
+++ supplemental/tracker/unity/jp/search/functions_0.js
@@ -1,8 +1,8 @@
 var searchData=
 [
-  ['ailiatrackeraddtarget_46',['ailiaTrackerAddTarget',['../classailiaTracker_1_1AiliaTracker.html#aa5940d018edeced77899dd49af229ad2',1,'ailiaTracker::AiliaTracker']]],
+  ['ailiatrackeraddtarget_46',['ailiaTrackerAddTarget',['../classailiaTracker_1_1AiliaTracker.html#a924df28f81b786081b2b8b0f86dc16f1',1,'ailiaTracker::AiliaTracker']]],
   ['ailiatrackercompute_47',['ailiaTrackerCompute',['../classailiaTracker_1_1AiliaTracker.html#a48d9607c923ca362776ff6ce6d4e763b',1,'ailiaTracker::AiliaTracker']]],
-  ['ailiatrackercreate_48',['ailiaTrackerCreate',['../classailiaTracker_1_1AiliaTracker.html#a2d0a0c1c226fe32d64a91d1d6ecc9620',1,'ailiaTracker::AiliaTracker']]],
+  ['ailiatrackercreate_48',['ailiaTrackerCreate',['../classailiaTracker_1_1AiliaTracker.html#a749ecfa96669fb15dab4b73e502ee2c2',1,'ailiaTracker::AiliaTracker']]],
   ['ailiatrackerdestroy_49',['ailiaTrackerDestroy',['../classailiaTracker_1_1AiliaTracker.html#a3d57c6d0d969a97da4bbee2acac7efa8',1,'ailiaTracker::AiliaTracker']]],
   ['ailiatrackergeterrordetail_50',['ailiaTrackerGetErrorDetail',['../classailiaTracker_1_1AiliaTracker.html#a25e399069b9fd2623182c2ff640b4d55',1,'ailiaTracker::AiliaTracker']]],
   ['ailiatrackergetobject_51',['ailiaTrackerGetObject',['../classailiaTracker_1_1AiliaTracker.html#a206ce034d9e5eba037cacf9894a60d75',1,'ailiaTracker::AiliaTracker']]],
